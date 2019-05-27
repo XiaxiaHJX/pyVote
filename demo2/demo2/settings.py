8198678449ae1e98d9b94fd5fdc7d3a9209d514d
xiaxia <1327870569@qq.com>
2019-05-27 19:05:06 +0800

    setting

--- demo2/demo2/settings.py
+++ demo2/demo2/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'y_ihuuh3o@3^*1119o*(=@69qd5g4p=^oiio88@d$i41nmq=gg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
